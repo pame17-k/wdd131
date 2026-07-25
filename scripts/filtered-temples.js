f7aa49306f7312eb85b9769b5cded6a7fb03649a
pame17-k <pbeltrantrujillo@byupathway.edu>
2026-07-24 19:15:34 -0600

    week4

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,12 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
+
+const menuButton = document.getElementById("menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  // Cambiar icono ☰ ↔ ❎
+  menuButton.textContent = navigation.classList.contains("open") ? "❎" : "☰";
+});
