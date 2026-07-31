bc227d1ea9dfa2e5e39f18c9df688902f5dacd78
pame17-k <pbeltrantrujillo@byupathway.edu>
2026-07-31 11:46:46 -0600

    w5pt4

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,41 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent =
   "Last modified: " + document.lastModified;
+
+// Product array
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+// Populate select options
+const productSelect = document.getElementById("productName");
+products.forEach(product => {
+  const option = document.createElement("option");
+  option.value = product.name;
+  option.textContent = product.name;
+  productSelect.appendChild(option);
+});
